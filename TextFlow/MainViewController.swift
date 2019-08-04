af584c24ebb20b868c3ac5284c90abdd3f46e604
Jahyun Oh <soharu1024@gmail.com>
2019-08-04 22:30:13 +0900

    [TextFlow] Add a new target

---
+++ TextFlow/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  TextFlow
+//
+//  Created by Jahyun Oh on 04/08/2019.
+//  Copyright © 2019 Jahyun Oh. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
