93b9a665e9b64080830ac8818dc2db60f04b6b8a
Jahyun Oh <soharu1024@gmail.com>
2019-10-30 20:45:04 +0900

    [Showcase] Add a new target

---
+++ Showcase/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Showcase
+//
+//  Created by Jahyun Oh on 2019/10/30.
+//  Copyright © 2019 Jahyun Oh. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGreen
+    }
+}
